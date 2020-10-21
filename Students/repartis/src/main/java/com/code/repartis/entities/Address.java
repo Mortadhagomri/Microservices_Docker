e453a2de374bd73c101cf9f84d0a5528731dbce6
bahREEE <bhrmitn@gmail.com>
2020-10-21 21:38:29 +0100

    usersFinal

--- Students/repartis/src/main/java/com/code/repartis/entities/Address.java
+++ Students/repartis/src/main/java/com/code/repartis/entities/Address.java
@@ -2,8 +2,6 @@ package com.code.repartis.entities;
 
 import javax.persistence.Embeddable;
 
-//import com.code.repartis.Fake.Geof;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,12 +17,5 @@ public class Address {
     private String city;
     private String zipcode;
     private Geo geo;
-
-/*
-    public void setGeo(Geof g){
-        this.geo.setLat(g.getLat());
-        this.geo.setLng(g.getLng());
-    }
-*/
 
 }
